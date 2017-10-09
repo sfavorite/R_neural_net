7c59a75e17e16c677a6fb07a140256126aec606a
Scot Favorite <scot.favorite@gmail.com>
2017-10-09 13:02:25 -0400

    Initial commit

---
+++ neural_net.R
@@ -0,0 +1,18 @@
+# Neural Network using back propagation 
+
+needed.libraries <- function() {
+      # functional will provide our optimization function: optim()  
+      library(functional)
+      source("randInitialWeights.R")
+      source("nnCost.R")
+      source("logistic_functions.R")
+      source("nnGrad.R")
+      source("predict.R")
+}
+
+neural_net <- function(x = x, y = y) {
+   
+      # Load need functions 
+      needed.ibraries() 
+      
+}
